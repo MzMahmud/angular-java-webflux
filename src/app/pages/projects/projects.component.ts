9a819d7c30e5cd5fa5afdfaa6f3207a46cc6d598
MzMahmud <moaz.mahmud.64@gmail.com>
2025-06-27 17:52:35 +0600

    added ag grid

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -4,10 +4,12 @@ import { AppState } from '../../store';
 import { Project } from '../../models/project.model';
 import { selectProjects } from '../../store/project/project.selector';
 import { loadAllProjects } from '../../store/project/project.action';
+import { AgGridAngular } from 'ag-grid-angular';
+import type { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './projects.component.html',
 })
 export class ProjectsComponent implements OnInit {
@@ -20,4 +22,15 @@ export class ProjectsComponent implements OnInit {
   ngOnInit(): void {
     this.store.dispatch(loadAllProjects());
   }
+
+  colDefs: ColDef<Project>[] = [
+    { field: 'name' },
+    { field: 'owner' },
+    { field: 'status' },
+  ];
+
+  defaultColDef: ColDef<Project> = {
+    flex: 1,
+    filter: true,
+  };
 }
