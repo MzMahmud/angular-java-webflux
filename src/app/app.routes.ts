a2e9e9b06513656739df98b77543fb437691fd04
MzMahmud <moaz.mahmud.64@gmail.com>
2025-06-27 15:51:00 +0600

    setup basic route structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '404', component: NotFoundComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'projects', pathMatch: 'full' },
+      { path: 'projects', component: ProjectsComponent, pathMatch: 'full' },
+      { path: '**', redirectTo: '404' },
+    ],
+  },
+  { path: '**', redirectTo: '404' },
+];
