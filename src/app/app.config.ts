0c595588b281db37792da7ac2bce95bd7f6fffeb
MzMahmud <moaz.mahmud.64@gmail.com>
2025-06-27 18:53:44 +0600

    pie chart setup added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
+import * as echarts from 'echarts/core';
+import { provideEchartsCore } from 'ngx-echarts';
+import { routes } from './app.routes';
 import { appStore } from './store';
 
 export const appConfig: ApplicationConfig = {
@@ -10,5 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore(appStore),
+    provideEchartsCore({ echarts }),
   ],
 };
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit, Signal } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AppState } from '../../store';
-import { Project } from '../../models/project.model';
-import { selectProjects } from '../../store/project/project.selector';
-import { loadAllProjects } from '../../store/project/project.action';
 import { AgGridAngular } from 'ag-grid-angular';
 import type { ColDef } from 'ag-grid-community';
+import { PieChart } from 'echarts/charts';
+import { LegendComponent, TooltipComponent } from 'echarts/components';
+import type { EChartsCoreOption } from 'echarts/core';
+import * as echarts from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
+import { NgxEchartsDirective } from 'ngx-echarts';
+import { Project } from '../../models/project.model';
+import { AppState } from '../../store';
+import { loadAllProjects } from '../../store/project/project.action';
+import { selectProjects } from '../../store/project/project.selector';
+
+echarts.use([PieChart, CanvasRenderer, TooltipComponent, LegendComponent]);
 
 @Component({
   selector: 'app-projects',
-  imports: [AgGridAngular],
+  imports: [AgGridAngular, NgxEchartsDirective],
   templateUrl: './projects.component.html',
 })
 export class ProjectsComponent implements OnInit {
@@ -32,5 +40,43 @@ export class ProjectsComponent implements OnInit {
   defaultColDef: ColDef<Project> = {
     flex: 1,
     filter: true,
+  };
+
+  options: EChartsCoreOption = {
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      orient: 'vertical',
+      top: 0,
+      right: 0,
+      align: 'right',
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '80%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2,
+        },
+        label: {
+          show: false,
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' },
+        ],
+      },
+    ],
   };
 }
